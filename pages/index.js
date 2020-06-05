36eda72723e52adb3450ec3f4d13b6b2cf0c3c95
vardan <Vardaan1992sharma@gmail.com>
2020-06-05 22:30:45 +0530

    added missing config files and minor change

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Create Next App</title>
+      <Head >
+        <title>Covid19-statistics</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -205,5 +205,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
